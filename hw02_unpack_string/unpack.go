8b142b365eeefb00e5ef44015184b533ea024625
pdovgol <pdovgol@mail.ru>
2024-09-15 12:19:28 +0300

    hw02 done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,49 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+// Проверить, что в строке нет повторяющихся цифр, цифра не первая и строка не пустая
+func isValid(runeStr []rune) bool {
+	if len(runeStr) == 0 {
+		return false
+	}
+	if unicode.IsDigit(runeStr[0]) {
+		return false
+	}
+	for i := 1; i < len(runeStr)-1; i++ {
+		if unicode.IsDigit(runeStr[i]) && unicode.IsDigit(runeStr[i+1]) {
+			return false
+		}
+	}
+	return true
+}
+
+func Unpack(inStr string) (string, error) {
+	if inStr == "" {
+		return "", nil
+	}
+	runeStr := []rune(inStr)
+	if !isValid(runeStr) {
+		return "", ErrInvalidString
+	}
+	var sb strings.Builder
+	for i := 0; i < len(runeStr); i++ {
+		if i == (len(runeStr) - 1) {
+			sb.WriteRune(runeStr[i])
+			break
+		}
+		//Если след. цифра, то добавить повторяющуюся строку и пропустить эту цифру в цикле
+		if unicode.IsDigit(runeStr[i+1]) {
+			sb.WriteString(strings.Repeat(string(runeStr[i]), int(runeStr[i+1])-'0'))
+			i++
+			continue
+		}
+		sb.WriteRune(runeStr[i])
+	}
+	return sb.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,12 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "asasa5", expected: "asasaaaaa"},
+		{input: "a1a1a1", expected: "aaa"},
+		{input: "a0a0a0", expected: ""},
+		{input: "@4", expected: "@@@@"},
+		{input: "!@#$%^&*/", expected: "!@#$%^&*/"},
+		{input: "a\t5e", expected: "a\t\t\t\t\te"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -34,7 +40,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "4"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
