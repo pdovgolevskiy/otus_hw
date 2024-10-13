d2433539d12cfc0f4c5d81d5c1b9008f1c70acfb
pdovgolevskiy <pdovgolevskiy@diasoft.ru>
2024-10-14 01:42:11 +0300

    [feat]: hw04 first impl

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -16,6 +16,32 @@ type lruCache struct {
 	items    map[Key]*ListItem
 }
 
+func (lruC *lruCache) Set(key Key, value interface{}) bool {
+	if _, ok := lruC.items[key]; ok {
+		lruC.items[key].Value = value
+		lruC.queue.MoveToFront(lruC.items[key])
+		return true
+	}
+	lruC.items[key] = lruC.queue.PushFront(value)
+	if lruC.queue.Len() > lruC.capacity {
+		delete(lruC.items, lruC.queue.Back().Value.(Key))
+		lruC.queue.Remove(lruC.queue.Back())
+	}
+	return false
+}
+
+func (lruC *lruCache) Get(key Key) (interface{}, bool) {
+	if value, ok := lruC.items[key]; ok {
+		return value.Value, ok
+	}
+	return 0, false
+}
+
+func (lruC *lruCache) Clear() {
+	lruC.items = make(map[Key]*ListItem)
+	lruC.queue = NewList()
+}
+
 func NewCache(capacity int) Cache {
 	return &lruCache{
 		capacity: capacity,
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,9 @@
 package hw04lrucache
 
+import (
+	"fmt"
+)
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -8,6 +12,7 @@ type List interface {
 	PushBack(v interface{}) *ListItem
 	Remove(i *ListItem)
 	MoveToFront(i *ListItem)
+	printList()
 }
 
 type ListItem struct {
@@ -16,9 +21,96 @@ type ListItem struct {
 	Prev  *ListItem
 }
 
+func initList(l *list, value interface{}) {
+	item := ListItem{Next: nil, Prev: nil, Value: value}
+	l.head, l.tail = &item, &item
+	l.elem_count++
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	i.Prev.Next = i.Next
+	i.Next.Prev = i.Prev
+
+	i.Prev = nil
+	i.Next = l.head
+	l.head.Prev = i
+	l.head = i
+	//TODO
+}
+
+func (l *list) Remove(i *ListItem) {
+	i.Prev.Next = i.Next
+	i.Next.Prev = i.Prev
+	l.elem_count--
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	if l.head == nil {
+		initList(l, v)
+		return l.head
+	}
+	var lItem = ListItem{Value: v, Prev: nil, Next: l.head}
+	l.head.Prev = &lItem
+	l.head = &lItem
+	l.elem_count++
+	return &lItem
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	if l.tail == nil {
+		initList(l, v)
+		return l.tail
+	}
+	var lItem = ListItem{Value: v, Prev: l.tail, Next: nil}
+	l.tail.Next = &lItem
+	l.tail = &lItem
+	l.elem_count++
+	return &lItem
+}
+
+func (l *list) Front() *ListItem {
+	if l.head == nil {
+		return nil
+	}
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	if l.tail == nil {
+		return nil
+	}
+	return l.tail
+}
+
+func (l *list) Len() int {
+	return l.elem_count
+}
+
 type list struct {
-	List // Remove me after realization.
+	//List       // Remove me after realization.
+	elem_count int
+	head       *ListItem
+	tail       *ListItem
 	// Place your code here.
+}
+
+func (l *list) printList() {
+	currentElem := l.head
+	for currentElem != nil {
+		fmt.Println(currentElem.Value)
+		currentElem = currentElem.Next
+	}
+}
+
+// removeByIndex удаляет элемент по индексу
+func removeByIndex(l *list, index int) {
+	currentElem := l.head
+	for i := 0; currentElem != nil; i++ {
+		if i == index {
+			l.MoveToFront(currentElem)
+		}
+		currentElem = currentElem.Next
+	}
 }
 
 func NewList() List {
