0dcda401c9632a17e89a1b08f8aaea4f0abed11e
Petr <pdovgol@mail.ru>
2024-10-27 17:40:08 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,34 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if len(tasks) < n {
+		n = len(tasks)
+	}
+	var errorCount int32
+	taskChannel := make(chan Task)
+	wg := sync.WaitGroup{}
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for currentTask := range taskChannel {
+				if err := currentTask(); err != nil {
+					atomic.AddInt32(&errorCount, 1)
+				}
+			}
+		}()
+	}
+	//
+	for _, task := range tasks {
+		if atomic.LoadInt32(&errorCount) >= int32(m) {
+			break
+		}
+		taskChannel <- task
+	}
+	close(taskChannel)
+	wg.Wait()
+	if atomic.LoadInt32(&errorCount) >= int32(m) {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,26 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("instant error limit", func(t *testing.T) {
+		tasksCount := 10
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+		workersCount := 10
+		maxErrorsCount := -23
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.LessOrEqual(t, runTasksCount, int32(workersCount), "extra tasks were started")
+	})
 }
