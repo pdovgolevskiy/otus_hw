24290edca1db2b2e402d78d7c0ce3bac2f6153b1
Petr <pdovgol@mail.ru>
2024-11-18 00:56:45 +0300

    hw07 part1

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"io"
+	"log"
+	"os"
 )
 
 var (
@@ -10,6 +13,45 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
+	const bufSize int = 1024
+	file, err := os.Open(fromPath)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fi, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sz := fi.Size()
+	if offset > sz {
+		log.Fatal(ErrUnsupportedFile)
+	}
+	//
+	mode := fi.Mode()
+	newFile, err := os.Create(toPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Chmod(toPath, mode)
+	//
+	//TMP
+	if limit == 0 || limit > sz {
+		limit = sz
+	}
+
+	for offset < sz {
+		io.CopyN(newFile, file, int64(bufSize))
+		// read, err := file.Read(buf[offset:])
+		// offset += int64(read)
+		// if err == io.EOF {
+		// // что если не дочитали ?
+		// break
+		// }
+		// if err != nil {
+		// log.Panicf("failed to read: %v", err)
+		// }
+	}
 	// Place your code here.
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -4,4 +4,5 @@ import "testing"
 
 func TestCopy(t *testing.T) {
 	// Place your code here.
+	// TODO существование файла, размер файла, что будет если файл уже существовал
 }
