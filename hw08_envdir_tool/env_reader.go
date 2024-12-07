a18387c1e0a7f37f2fff77ebe4db2b66a82fe7bd
Petr <pdovgol@mail.ru>
2024-12-07 14:16:20 +0300

    hw08 start

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +15,43 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+func readLine(dir string) {
+	file, err := os.Open(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	env := Environment{}
+	for _, e := range entries {
+		file, err := os.Open(e.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+		scanner := bufio.NewScanner(file)
+		scanner.Scan()
+		file.Close()
+		env[e.Name()] = EnvValue{scanner.Text(), false}
+		//os.Setenv(e.Name(), strings.TrimRight(scanner.Text(), " 	"))
+		// env.key = e.Name() env.val = scanner.Text()
+	}
 	return nil, nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,6 @@
 package main
 
 func main() {
+	ReadDir("./testdata/env")
 	// Place your code here.
 }
