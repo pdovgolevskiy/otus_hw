24775fb1923a4f7bfe8f3b8fc1b79ec2a27cd16c
Petr <pdovgol@mail.ru>
2024-12-16 01:28:49 +0300

    initial hw09

--- hw08_envdir_tool/testdata/echo.sh
+++ hw08_envdir_tool/testdata/echo.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 echo -e "HELLO is (${HELLO})
 BAR is (${BAR})
 FOO is (${FOO})
--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,9 +1,21 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
 }
+
+var (
+	ErrUnsupportedOperation = errors.New("unsupported validation")
+	ErrInvalidRule          = errors.New("invalid rule")
+)
 
 type ValidationErrors []ValidationError
 
@@ -11,7 +23,29 @@ func (v ValidationErrors) Error() string {
 	panic("implement me")
 }
 
+func findRulesFromTag(validateFullStr string) {
+	rules := strings.Split(validateFullStr, "|")
+	for _, rule := range rules {
+		rl := strings.Split(rule, ":")
+
+	}
+}
+
 func Validate(v interface{}) error {
+	st := reflect.TypeOf(v)
+	for i := 0; i < st.NumField(); i++ {
+		field := st.Field(i)
+		if validateVal, ok := field.Tag.Lookup("validate"); ok {
+			if validateVal == "" {
+				continue
+				//TODO
+			} else {
+				findRulesFromTag(validateVal)
+			}
+		} else {
+			fmt.Println("(not specified)")
+		}
+	}
 	// Place your code here.
 	return nil
 }
